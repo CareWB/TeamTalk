16b7107d1b64c5fa3d036c6b6c32badbe696f50b
WB <wb@WBdeMacBook-Pro.local>
2016-12-18 20:43:58 +0800

    rsp struct add attach_data field

--- server/src/msg_server/DBServConn.h
+++ server/src/msg_server/DBServConn.h
@@ -49,6 +49,7 @@ private:
     
     void _HandlePushShieldResponse(CImPdu* pPdu);
     void _HandleQueryPushShieldResponse(CImPdu* pPdu);
+    void _HandleCreateTravelResponse(CImPdu* pPdu);
 private:
 	bool 		m_bOpen;
 	uint32_t	m_serv_idx;
